b7acda8baa5c8693ccb1b87cdecce6c254f6cc51
özcan çetin <cetin445@gmail.com>
2022-08-25 18:14:56 +0300

    model and get method-v1

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { DataserviceService } from 'src/app/dataservice.service';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+  TodosList:Todo[]=[];
+
+  constructor(private service:DataserviceService) {
+    service.getTodos().subscribe(res=>{
+      this.TodosList=res;
+    })
+   }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/dataservice.service.ts
+++ src/app/dataservice.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Todo } from './Todo';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataserviceService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  _url = 'https://jsonplaceholder.typicode.com/';
+
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this._url + 'todos');
+  }
 }
